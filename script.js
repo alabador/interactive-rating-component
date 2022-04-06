56e9b91dfbcdcaa6013830ae477edde21706e3ae
Austin <austinlabador@gmail.com>
2022-04-06 01:02:17 -0700

    removed old code and added comments

--- script.js
+++ script.js
@@ -10,28 +10,12 @@ const darkBlue = styles.getPropertyValue('--dark-blue');
 
 for (let button of buttons) {    
     button.addEventListener('click', function(e) {
+        //iterates through buttons and removes active state
         buttons.forEach(button => {
             button.classList.remove('orange');
         });
-
+        //adds active state to current button
         e.target.classList.add('orange');
 
-        // console.log(getComputedStyle(button).backgroundColor);
-        // if (getComputedStyle(button).backgroundColor == darkBlue) {
-        //     button.style.backgroundColor = orange;
-        //     button.style.color = '#ffffff';
-        // }
-
-        // else if (getComputedStyle(button).backgroundColor == orange) {
-        //     button.style.backgroundColor = darkBlue;
-        //     button.style.color = mediumGray;
-        // }
     })
 } 
-
-// for (let i=0; i<buttons.length; i++) {
-//     buttons[i].addEventListener('click', function() {
-//         this.classList.add('orange');
-//         let currentButton = i;
-//     })
-// }
