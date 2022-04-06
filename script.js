3b1af81b1f4ae453a0386d46a428b6576769eb61
Austin <austinlabador@gmail.com>
2022-04-06 15:07:51 -0700

    choosing a button now reflects the value on the thank you screen

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const buttons = document.querySelectorAll('#button-container .button');
 const submit = document.querySelector('.submit');
 const rate = document.querySelector('#rate-us');
 const thanks = document.querySelector('#thank-you');
+const ratingValue = document.querySelector('#ratingValue');
+let value = 0;
 
 // get and set style declarations from css
 const styles = window.getComputedStyle(document.body);
 const orange = styles.getPropertyValue('--orange');
 const mediumGray = styles.getPropertyValue('--medium-gray');
 const darkBlue = styles.getPropertyValue('--dark-blue');
-
 
 for (let button of buttons) {    
     button.addEventListener('click', function(e) {
@@ -19,6 +20,8 @@ for (let button of buttons) {
         });
         //adds active state to current button
         e.target.classList.add('orange');
+        value = button.value;
+        ratingValue.innerText = value;
     })
 } 
 
