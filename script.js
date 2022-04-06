1a24b2365fed40ac772ad3c6096abc3671939a33
Austin <austinlabador@gmail.com>
2022-04-05 23:23:20 -0700

    reworked logic for event listener

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+//button variables
 const buttons = document.querySelectorAll('#button-container .button');
+const button1 = document.querySelector('#button-1');
+const button2 = document.querySelector('#button-2');
+const button3 = document.querySelector('#button-3');
+const button4 = document.querySelector('#button-4');
+const button5 = document.querySelector('#button-5');
 
 // get and set style declarations from css
 const styles = window.getComputedStyle(document.body);
@@ -7,11 +13,28 @@ const mediumGray = styles.getPropertyValue('--medium-gray');
 const darkBlue = styles.getPropertyValue('--dark-blue');
 
 
+
 for (let button of buttons) {    
     button.addEventListener('click', function() {
+        // console.log(getComputedStyle(button).backgroundColor);
+        if (getComputedStyle(button).backgroundColor == mediumGray) {
+            console.log('this works');
+            button.style.backgroundColor = orange;
+            button.style.color = '#ffffff';
+        }
 
-        
-        button.style.backgroundColor = `${orange}`;
-        button.style.color = '#ffffff'
+
+        // if (`'${getComputedStyle(button).backgroundColor}'` == darkBlue.trim()) {
+        //     console.log('this works');
+        //     button.style.backgroundColor = `${orange}`;
+        //     button.style.color = '#ffffff'
+        // }
+
+        // else {
+        //     console.log('this does not work');
+        // }
+
+        // button.style.backgroundColor = `${orange}`;
+        // button.style.color = '#ffffff'
     })
 } 
